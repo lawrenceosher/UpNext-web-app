77abb714fb6dcfe0812350884649964d27b0abb5
Lawrence Osher <lao2205@gmail.com>
2025-02-18 20:43:58 -0500

    overriding default styles

--- upnext-web-app/src/App.tsx
+++ upnext-web-app/src/App.tsx
@@ -1,18 +1,18 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router";
 import UpNext from "./UpNext";
+import "./App.css";
 
 function App() {
-
   return (
     <HashRouter>
       <div>
-      <Routes>
-          <Route path="/" element={<Navigate to="UpNext"/>} />
+        <Routes>
+          <Route path="/" element={<Navigate to="UpNext" />} />
           <Route path="/UpNext/*" element={<UpNext />} />
         </Routes>
       </div>
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
