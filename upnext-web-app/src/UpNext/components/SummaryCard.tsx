0ef0b8bfda9926a07927fb8054fd60dadfb18b22
Lawrence Osher <lao2205@gmail.com>
2025-02-19 18:02:20 -0500

    started summary card

---
+++ upnext-web-app/src/UpNext/components/SummaryCard.tsx
@@ -0,0 +1,9 @@
+import "./SummaryCard.css";
+
+export default function SummaryCard() {
+  return (
+    <div className="bg-transparent border border-2 mt-4 flex-grow-1 card-border">
+      Placeholder
+    </div>
+  );
+}
--- upnext-web-app/src/UpNext/pages/Movies.tsx
+++ upnext-web-app/src/UpNext/pages/Movies.tsx
@@ -1,12 +1,16 @@
 import { Col, Container, Row } from "react-bootstrap";
 import MediaSearch from "../components/MediaSearch";
+import SummaryCard from "../components/SummaryCard";
 
 export default function Movies() {
   return (
     <Container className="text-center">
       <Row>
         <Col>Column</Col>
-        <Col><MediaSearch /></Col>
+        <Col>
+          <MediaSearch />
+          <SummaryCard />
+        </Col>
       </Row>
     </Container>
   );
