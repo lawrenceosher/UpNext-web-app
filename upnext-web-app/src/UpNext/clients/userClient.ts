83a9f2055c3d04526743d9561acbb6c4ffb9ec92
Lawrence Osher <lao2205@gmail.com>
2025-04-12 21:28:20 -0400

    implemented login and error reducer

--- upnext-web-app/src/UpNext/clients/userClient.ts
+++ upnext-web-app/src/UpNext/clients/userClient.ts
@@ -8,3 +8,16 @@ export const signup = async (user: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
   return response.data;
 };
+
+export const signin = async (credentials: any) => {
+  try {
+    const response = await axiosWithCredentials.post(
+      `${USERS_API}/login`,
+      credentials
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error during sign-in:", error);
+    return null;
+  }
+};
--- upnext-web-app/src/UpNext/store/index.ts
+++ upnext-web-app/src/UpNext/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "../redux/accountReducer";
+import errorReducer from "../redux/errorReducer";
 
 const store = configureStore({
   reducer: {
     accountReducer,
+    errorReducer,
   },
 });
 
