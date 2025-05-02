6fd71734743d1d8d1ec7f22bb18f67113ec32f6a
Lawrence Osher <lao2205@gmail.com>
2025-05-02 17:38:15 -0400

    Created useUsersPage hook

--- upnext-web-app/src/UpNext/utils.ts
+++ upnext-web-app/src/UpNext/utils.ts
@@ -11,4 +11,18 @@ const readableDateJoined = (date: string) => {
   });
 };
 
-export { readableDateJoined };
+/**
+ * Converts an ISO date string to a readable format in MM/DD/YYYY
+ * @param isoDateString - The ISO date string to be converted
+ * @returns A string representing the date in a readable format
+ */
+function formatReadableDate(isoDateString: string) {
+  const myDate = new Date(isoDateString);
+
+  const year = myDate.getFullYear();
+  const month = String(myDate.getMonth() + 1).padStart(2, "0");
+  const day = String(myDate.getDate()).padStart(2, "0");
+  return `${month}/${day}/${year}`;
+}
+
+export { readableDateJoined, formatReadableDate };
