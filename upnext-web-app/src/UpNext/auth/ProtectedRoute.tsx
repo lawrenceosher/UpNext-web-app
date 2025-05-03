5e27e80546d684067d9842210020b650978fc4c9
Lawrence Osher <lao2205@gmail.com>
2025-05-03 19:38:58 -0400

    Comments for protected route and session

--- upnext-web-app/src/UpNext/auth/ProtectedRoute.tsx
+++ upnext-web-app/src/UpNext/auth/ProtectedRoute.tsx
@@ -1,8 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+
+/**
+ * ProtectedRoute component to protect routes that require authentication
+ * @param children The child components to render if the user is authenticated
+ */
 export default function ProtectedRoute({ children }: { children: any }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+
+  // Check if the user is authenticated
+  // If authenticated, render the children components
+  // If not authenticated, redirect to the login page
   if (currentUser) {
     return children;
   } else {
--- upnext-web-app/src/UpNext/auth/Session.tsx
+++ upnext-web-app/src/UpNext/auth/Session.tsx
@@ -3,21 +3,30 @@ import * as userClient from "../clients/userClient";
 import { useEffect, useState } from "react";
 import { setCurrentUser } from "../redux/accountReducer";
 import { useDispatch } from "react-redux";
+
+/**
+ * Session component to fetch and set the current user session
+ * @param children The child components to render after fetching the user session
+ */
 export default function Session({ children }: { children: any }) {
   const [pending, setPending] = useState(true);
   const dispatch = useDispatch();
-  const fetchProfile = async () => {
-    try {
-      const currentUser = await userClient.getProfile();
-      dispatch(setCurrentUser(currentUser));
-    } catch (err: any) {
-      console.error(err);
-    }
-    setPending(false);
-  };
+
   useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const currentUser = await userClient.getProfile();
+        
+        dispatch(setCurrentUser(currentUser));
+      } catch (err: any) {
+        console.error(err);
+      }
+      setPending(false);
+    };
+
     fetchProfile();
-  }, []);
+  }, [dispatch]);
+
   if (!pending) {
     return children;
   }
