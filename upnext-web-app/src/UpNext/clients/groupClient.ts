29e82e43478d70b7accde5bbcc43078cb07f62f3
Lawrence Osher <lao2205@gmail.com>
2025-05-03 22:04:57 -0400

    Added comments to all client files

--- upnext-web-app/src/UpNext/clients/groupClient.ts
+++ upnext-web-app/src/UpNext/clients/groupClient.ts
@@ -3,11 +3,12 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export const GROUPS_API = `${REMOTE_SERVER}/api/groups`;
 
-export const getAllGroups = async () => {
-  const response = await axiosWithCredentials.get(`${GROUPS_API}`);
-  return response.data;
-};
-
+/**
+ * Creates a new group with the given name and creator.
+ * @param groupName The name of the group to be created
+ * @param creator The username of the user creating the group
+ * @returns The created group object
+ */
 export const createGroup = async (groupName: string, creator: string) => {
   const response = await axiosWithCredentials.post(`${GROUPS_API}`, {
     groupName,
@@ -16,6 +17,11 @@ export const createGroup = async (groupName: string, creator: string) => {
   return response.data;
 };
 
+/**
+ * Deletes a group with the given ID.
+ * @param groupId The ID of the group to be deleted
+ * @returns The deleted group object
+ */
 export const deleteGroup = async (groupId: string) => {
   const response = await axiosWithCredentials.delete(
     `${GROUPS_API}/${groupId}`
@@ -23,6 +29,12 @@ export const deleteGroup = async (groupId: string) => {
   return response.data;
 };
 
+/**
+ * Updates the name of a group with the given ID.
+ * @param groupId The ID of the group to be updated
+ * @param groupName The new name of the group
+ * @returns The updated group object
+ */
 export const updateGroup = async (
   groupId: string,
   groupName: string,
@@ -33,11 +45,22 @@ export const updateGroup = async (
   return response.data;
 };
 
+/**
+ * Retrieves a list of groups for a given user.
+ * @param username The username of the user whose groups are to be fetched
+ * @returns A list of groups the user is a member of
+ */
 export const getGroupsForUser = async (username: string) => {
   const response = await axiosWithCredentials.get(`${GROUPS_API}/${username}`);
   return response.data;
 };
 
+/**
+ * Removes a user from a group with the given ID. Also used for a user to leave a group.
+ * @param groupId The ID of the group to be updated
+ * @param username The username of the user to be removed
+ * @returns The updated group object
+ */
 export const removeGroupMember = async (groupId: string, username: string) => {
   const response = await axiosWithCredentials.put(`${GROUPS_API}/${groupId}/remove`, {
     username,
--- upnext-web-app/src/UpNext/clients/userClient.ts
+++ upnext-web-app/src/UpNext/clients/userClient.ts
@@ -4,6 +4,11 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+/**
+ * Creates a new user with the given information.
+ * @param user The user object containing the user's information
+ * @returns The created user object
+ */
 export const signup = async (user: any) => {
   try {
     const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
@@ -13,9 +18,13 @@ export const signup = async (user: any) => {
       throw new Error(`${error.response?.data}`);
     }
   }
-  
 };
 
+/**
+ * Logs in a user with the given credentials.
+ * @param credentials The credentials object containing the user's login information
+ * @returns The logged-in user object
+ */
 export const signin = async (credentials: any) => {
   try {
     const response = await axiosWithCredentials.post(
@@ -29,6 +38,10 @@ export const signin = async (credentials: any) => {
   }
 };
 
+/**
+ * Logs out the currently logged-in user.
+ * @returns The logged-in user object
+ */
 export const signout = async () => {
   try {
     const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
@@ -39,26 +52,50 @@ export const signout = async () => {
   }
 };
 
+/**
+ * Retrieves a list of all users
+ * @returns A list of all users
+ */
 export const getAllUsers = async () => {
   const response = await axiosWithCredentials.get(`${USERS_API}`);
   return response.data;
 }
 
+/**
+ * Deletes a user with the given ID.
+ * @param userId The ID of the user to be deleted
+ * @returns The deleted user object
+ */
 export const deleteUser = async (userId: string) => {
   const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}`);
   return response.data;
 }
 
+/**
+ * Retrieves the profile of the currently logged-in user.
+ * @returns The logged-in user object
+ */
 export const getProfile = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
   return response.data;
 }
 
+/**
+ * Retrieves a user by their ID.
+ * @param userId The ID of the user to be retrieved
+ * @returns The user object
+ */
 export const getUserById = async (userId: string) => {
   const response = await axiosWithCredentials.get(`${USERS_API}/${userId}`);
   return response.data;
 }
 
+/**
+ * Updates a user with the given ID.
+ * @param userId The ID of the user to be updated
+ * @param user The user object containing the updated information
+ * @returns The updated user object
+ */
 export const updateUser = async (userId: string, user: any) => {
   const response = await axiosWithCredentials.put(`${USERS_API}/${userId}`, user);
   return response.data;
