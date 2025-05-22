5294898d2c3acd891cfdf13f4da17dbe6a101a26
Lawrence Osher <lao2205@gmail.com>
2025-05-22 17:32:09 -0400

    mobile styling for profile

--- src/UpNext/components/UserSummary.tsx
+++ src/UpNext/components/UserSummary.tsx
@@ -1,17 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { MdDateRange } from "react-icons/md";
+import {
+  MdDateRange,
+  MdNotificationsNone,
+  MdOutlineSettings,
+} from "react-icons/md";
 import { readableDateJoined } from "../utils";
 
 /**
- * Display the user's username, date joined, groups, and history summary with 
+ * Display the user's username, date joined, groups, and history summary with
  * values for how much of each media type they have consumed.
  * @param userData - The user data object containing user information and queues.
  */
-export default function UserSummary({ userData }: { userData: any }) {
+export default function UserSummary({
+  userData,
+  handleShowAccountSettings,
+  handleShowNotifcations,
+  isViewingOwnProfile,
+}: {
+  userData: any;
+  handleShowAccountSettings: () => void;
+  handleShowNotifcations: () => void;
+  isViewingOwnProfile: boolean;
+}) {
   return (
     <>
       <div className="d-flex align-items-center">
-        <h1 className="fw-bold display-2">{userData.username}</h1>
+        <h1 className="fw-bold display-2">
+          {userData.username}{" "}
+          {isViewingOwnProfile && (
+            <div className="d-inline-block d-sm-none">
+              <MdOutlineSettings
+                id="icon-button"
+                className="display-5 float-end"
+                onClick={handleShowAccountSettings}
+              />
+              <MdNotificationsNone
+                id="icon-button"
+                className="display-5 float-end me-2"
+                onClick={handleShowNotifcations}
+              />
+            </div>
+          )}
+        </h1>
       </div>
 
       <h4>
